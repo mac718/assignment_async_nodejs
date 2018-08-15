c9848c4d13bb3ee0f1e0ee49b39692523b822e83
mac718 <mac718@gmail.com>
2018-08-15 07:36:32 -0400

    Emiitter module

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 var promises = require('./lib/promises');
 var fsp = require('./lib/fsp');
+var Emitter = require('./lib/emitter');
+var eventEmitter = new Emitter();
 
-// promises.hello.then(function(result){
-//   console.log(result);
-// });
+promises.hello.then(function(result){
+  console.log(result);
+});
 
-// promises.delay(1000)
-//   .then(promises.countDown) //=> 1000
-//   .then(promises.countDown) //=> 900
-//   .then(promises.countDown) //=> 800
-//   .then(promises.countDown) //=> 700
-//   .then(promises.countDown) //=> 600
-//   .then(promises.countDown) //=> 500
-//   .then(promises.countDown) //=> 400
-//   .then(promises.countDown) //=> 300
-//   .then(promises.countDown) //=> 200
-//   .then(promises.countDown) //=> 100
-//   .then(promises.countDown); //=> Done!
+promises.delay(1000)
+  .then(promises.countDown) //=> 1000
+  .then(promises.countDown) //=> 900
+  .then(promises.countDown) //=> 800
+  .then(promises.countDown) //=> 700
+  .then(promises.countDown) //=> 600
+  .then(promises.countDown) //=> 500
+  .then(promises.countDown) //=> 400
+  .then(promises.countDown) //=> 300
+  .then(promises.countDown) //=> 200
+  .then(promises.countDown) //=> 100
+  .then(promises.countDown); //=> Done!
 
-// let arr = [1, 2, 3, 4];
+let arr = [1, 2, 3, 4];
 
-// arr = arr.map((elem) => {
-//   return promises.square(elem);
-// });
+arr = arr.map((elem) => {
+  return promises.square(elem);
+});
 
-// Promise.all(arr).then((result) => {
-//   console.log(result);
-// });
+Promise.all(arr).then((result) => {
+  console.log(result);
+});
 
-// promises.doBadThings(true).then((result) => {
-//   console.log(result)
-//   throw "D'oh!";
-// }).catch((err) => {
-//   console.error(err);
-// });
+promises.doBadThings(true).then((result) => {
+  console.log(result)
+  throw "D'oh!";
+}).catch((err) => {
+  console.error(err);
+});
 
-// promises.doBadThings(false).then((result) => {
-//   console.log(result)
-// }, (err) => {
-//   console.error(err);
-// });
+promises.doBadThings(false).then((result) => {
+  console.log(result)
+}, (err) => {
+  console.error(err);
+});
 
 fsp.readFile('./lib/dummy.txt').then((result) => {
   debugger;
@@ -65,3 +67,5 @@ fsp.writeFile('./lib/dummy.txt', 'Yeah!')
   .catch(function(err) {
     console.error(err);
   });
+
+
